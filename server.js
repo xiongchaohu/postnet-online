bea6d9bc5fab45568af02e630f26bd4e8ff4e94b
xiongchaohu <1732139715@qq.com>
2016-08-09 11:42:03 +0800

    modify server

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const bodyParser=require('body-parser');
 const barcodeTransformer=require('./public/core/BarcodeTransformer');
 var express = require('express');
 var app = express();
-//var cors=require('cors');
 app.use(bodyParser.urlencoded({extend:true}));
 app.use(express.static('public'));
-//app.use(cors());
 
 app.post('/postTransformer', function(req, res){
     res.send(postTransformer.postToBarcode(req.body.cmd));
