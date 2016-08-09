8a5fa84a6c772dc22d2a296685d131c775c72883
xiongchaohu <1732139715@qq.com>
2016-08-09 11:42:57 +0800

    modify webjs

--- public/core/webjs.js
+++ public/core/webjs.js
@@ -28,25 +28,10 @@ $(function () {
         $('#error').hide();
     });
 
-    /*function history(done) {
-        postRequest($("#inputCode"));
-        setTimeout(function () {
-            if ($('tr').length > 10) {
-                $('tr:last').remove();
-            }
-            if ($('#error').html() === '') {
-                var tr = $(`<tr><td>${$("#inputCode").val()}</td><td>${$('#data').html()}</td></tr>`);
-                tr.prependTo($('tbody'));
-            }
-
-        }, 100);
-
-    }*/
 
     $('button').click(function () {
         $('#transformResult').show();
         $('#error').show();
-        //history();
         postRequest($("#inputCode"));
     });
 
